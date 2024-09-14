c49700bf7df293333d05896e6b8a8f1645ae5ed8
kevinroosey <kevinroosey@gmail.com>
2024-09-13 21:56:59 -0500

    update

--- scripts/ticker-to-cik/main.py
+++ scripts/ticker-to-cik/main.py
@@ -1,6 +1,8 @@
 from sec_cik_mapper import StockMapper
 from pathlib import Path
-
+'''
+    Unfortunately, SEC edgar data requires CIK instead of ticker symbols so we need to map the ticker symbols to CIKs so API route can use ticker symbols instead of ciks
+'''
 def main():
     stock_mapper = StockMapper()
     stock_mapper.ticker_to_cik
